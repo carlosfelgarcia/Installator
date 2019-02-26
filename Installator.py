f5e7fcd1e89859952a638e5d77e3d78530d90a99
carlos garcia <carlos@carlosgarcia.com>
2019-02-25 18:31:22 -0800

    Zip download and extracted working

--- Installator.py
+++ Installator.py
@@ -1,4 +1,7 @@
 """Main module that will orchestate the installation process."""
+import sys
+import os
+import Download
 
 
 class Installator(object):
@@ -9,7 +12,7 @@ class Installator(object):
 
         Args:
             url (str):  The url to download the package to install.
-            destination (str): Where to install the package..
+            destination (str): Where to install the package.
             currentVersion (int): Current version of the app to be compared with the downloaded package. Defaults to 0.
             sshKey (type): If the system needs a key to access. Defaults to None.
         """
@@ -17,4 +20,26 @@ class Installator(object):
         self.__destination = destination
         self.__currentVersion = currentVersion
 
-        
+        self.__downloadModule = Download.Download(self.__url)
+
+    def install(self):
+        """Install the file located in the url given."""
+        self.__downloadModule.download()
+        print('Downloaded Files ----> ', self.__downloadModule.getDownloadedFiles())
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    removeTmp = False
+    if len(args) == 1:
+        url = 'https://github.com/carlosfelgarcia/rvtools/archive/0.1.2.zip'
+        dest = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tmp')
+        removeTmp = True
+    else:
+        url = args[1]
+        dest = args[2]
+    print('URL - {url}'.format(url=url))
+    print('Dest - {dest}'.format(dest=dest))
+    # print('Version - {ver}'.format(ver=args[3]))
+    app = Installator(url, dest)
+    app.install()
